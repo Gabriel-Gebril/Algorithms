fa904e835deaf8ccfb9d4a3261e3fe488c4d53cf
Gabriel Gebril <Gebril.Gabriel@gmail.com>
2019-03-04 22:55:19 -0500

    Added comments to Add_Two_Numbers.py

--- Add_Two_Numbers.py
+++ Add_Two_Numbers.py
@@ -21,9 +21,12 @@ def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
             y = q.val
         else:
             y = 0
-            
+        
         s = carry + x + y
+        # This is where the magic happens! We encode the carry value the cary is going to be integer division
+        # of the sum (we get the first digit or zero)
         carry = s // 10
+        # The if we have a carry value we just take the second digits (this is why we mod 10 to cut off the front digit if we have carry over)
         r.next = ListNode(s % 10)
         
         r = r.next
@@ -37,4 +40,6 @@ def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
     if carry > 0:
         r.next = ListNode(carry)
     
+    # We basically padded the return with a 0 to make the rest of the algorithm simpler.
+    # This way we don't have to worry about the current value of r just what it will be later.
     return dl.next
